74b14dde6f047d4875ca1bb9bb7c3a572d578518
wkeai <wkeai8888@gmail.com>
2023-06-15 15:12:06 +0800

    first commit

---
+++ static/js/Home-c158b776.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t}from"./index-0571a97c.js";const c={},r={class:"text-white dark:text-red-500"};function s(a,n){return o(),t("div",r," Home ")}const l=e(c,[["render",s],["__file","/Users/wangkeai/work/xueyan/followx/followx-dapp/src/view/home/Home.vue"]]);export{l as default};
